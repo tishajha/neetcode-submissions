eec85ba53f5d627b5a5e8421d682ddf0ead0274e
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-05-24 03:39:54 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size()+3, 0);
+        dp[cost.size()]=0;
+        for(int i= cost.size()-1; i>=0;i-- ){
+            dp[i]= min(dp[i+1]+cost[i],dp[i+2]+cost[i]);
+        }
+        return min(dp[0],dp[1]);
+    }
+};
