a7214aa936d1d97d79ea42baea873e3c2bbd27b7
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-07-03 01:48:34 +0530

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.java
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+       Collections.sort(intervals, (a,b)->Integer.compare(a.start,b.start));
+       PriorityQueue<Integer>minheap= new PriorityQueue<>();
+       for(Interval i:intervals){
+        if(!minheap.isEmpty()&& minheap.peek()<=i.start){
+            minheap.poll();
+        }
+        minheap.offer(i.end);
+       }
+       return minheap.size();
+    }
+}
