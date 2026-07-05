41c2a49d06544e54671d24b3b6af264dd05e0be0
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-07-05 18:58:53 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count= new int[3];
+        for(int n:nums){
+            count[n]++;
+        }
+        int index=0;
+        for(int i=0;i<3;i++){
+            while (count[i]>0){
+                nums[index]=i;
+                count[i]--;
+                index++;
+            }
+        }
+    }
+}
