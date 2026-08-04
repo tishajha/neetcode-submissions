a4064a556334ae6c72b3d70b5579232aa34c30c9
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-08-05 02:23:12 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxjump=0;
+        for(int i=0;i<nums.size(); i++){
+            if(i>maxjump){
+                return false;
+            }
+            maxjump= max(maxjump, i+nums[i]);
+        }
+        return true;
+    }
+};
