46aa9dc87665231b37d6bc20f2c1bf460465b557
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-08-08 05:25:35 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res=0; int l=0; int r=0;
+        while(r<nums.size()-1){
+            int far=0;
+            for(int i=l; i<=r; i++){
+                far=max(far, i+nums[i]);
+            }
+            l=r+1; r=far;
+            res++;
+        }
+        return res;
+    }
+};
