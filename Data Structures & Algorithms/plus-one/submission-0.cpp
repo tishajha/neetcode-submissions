f1ec79dcb16b989a5fca43f2a252c473757f8044
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-08-14 04:38:57 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size()-1;
+        for(int i=n; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
