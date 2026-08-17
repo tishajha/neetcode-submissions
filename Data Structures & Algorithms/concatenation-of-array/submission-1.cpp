6616a1d03a81e802d0edbb49bece3e51be39dedd
Tisha Jha <61746353+tishajha@users.noreply.github.com>
2026-08-17 12:19:27 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans ;
+        ans= nums;
+        for(int i=0; i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
